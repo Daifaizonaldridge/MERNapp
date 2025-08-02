e4fc4f4a797a16f09f295a6a94a7f847bad6b1b0
Daifaizon J Aldridge <daifaizonaldridge@gmail.com>
2025-08-02 17:19:06 -0500

    added nodemon

--- Backend/server.js
+++ Backend/server.js
@@ -1,7 +1,12 @@
-//import express from 'express';
-const express = require("express");
-
+import express from 'express';
+//const express = require("express");
 const app = express();
+
+app.get("/api/notes", (req, res) => {
+    // Send the notes
+    res.status(200).send("You got 22 notes");
+});
+
 
 
 app.listen(5001, () => {
