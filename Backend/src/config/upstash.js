720e46910e8c7d14f96369f850fb5c6ac47f9ba4
Daifaizon J Aldridge <daifaizonaldridge@gmail.com>
2025-08-18 22:20:38 -0500

    fixed it

--- Backend/src/config/upstash.js
+++ Backend/src/config/upstash.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// create a ratelimiter that allows 100 per minute
+// create a ratelimiter that allows 100s per minute
 const ratelimit = new Ratelimit({
     redis: Redis.fromEnv(),
     limiter: Ratelimit.slidingWindow(100, "60 s"),
