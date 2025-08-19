612ad8e448180509a2f034a244f5872fcc051e71
Daifaizon J Aldridge <daifaizonaldridge@gmail.com>
2025-08-18 21:11:34 -0500

    added code to make deployment of this respository ready.

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,20 +4,26 @@ import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
-
+import path from "path";
 
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 
 
 //middleware
-app.use(cors({
-  origin:"http://localhost:5173",
-}));
+if(process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin:"http://localhost:5173",
+    })
+  );
+}
+
 app.use(express.json()); //this middleware will parse the JSON bodies
 app.use(rateLimiter);
 
@@ -28,6 +34,14 @@ app.use((req,res,next) => {
 });
 
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname,"../Frontend/dist")));
+
+  app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"../Frontend","dist","index.html"));
+});
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
